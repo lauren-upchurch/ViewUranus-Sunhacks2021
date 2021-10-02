9bf72bb5dd26f466f34fc8986bfce74cd321cd91
unknown <Gjednesty@gmail.com>
2021-10-02 13:46:51 -0400

    Add tests

--- Uranus/src/MoonPhase.java
+++ Uranus/src/MoonPhase.java
@@ -1,6 +1,3 @@
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 import java.time.LocalDate;
 import java.time.Month;
 //import java.util.TimeZone;
@@ -12,12 +9,11 @@ public class MoonPhase {
     private Month currentMonth;
     private int currentYear;
     private final LocalDate newMoon = LocalDate.of(2000, Month.JANUARY, 6);
-    private
 
     public MoonPhase() {
         this.currentDate = LocalDate.now();
-        this.currentDay = currentdate.getDayOfMonth();
-        this.currentMonth = currentdate.getMonth();
+        this.currentDay = currentDate.getDayOfMonth();
+        this.currentMonth = currentDate.getMonth();
         this.currentYear = currentDate.getYear();
     }
 
@@ -32,22 +28,24 @@ public class MoonPhase {
         int b = a / 4;
         int c = 2 - a + b;
         int d = currentDay;
-        int e = 365.25 * (year + 4716);
-        int f = 30.60001 * (month + 1);
+        int e = (int) (365.25 * (year + 4716));
+        int f = (int) (30.60001 * (month + 1));
         double j = c + d + e + f - 1524.5; // Julian day
         double sinceNewMoon  = j - 2451549.5;
         double newMoons = sinceNewMoon / 29.53;
         //double lastNewMoon = Math.ceil(getDecimal(newMoons));
         double lastNewMoon = getDecimal(newMoons) * 29.53;
-        
+        double nextNewMoon = 29.5 - lastNewMoon;
+        return currentDate.plusDays((long) nextNewMoon);
     }
 
     private double getDecimal(double doubleNumber) {
         String doubleAsString = String.valueOf(doubleNumber);
         int indexOfDecimal = doubleAsString.indexOf(".");
-        System.out.println("Integer Part: " + doubleAsString.substring(0, indexOfDecimal));
-        System.out.println("Decimal Part: " + doubleAsString.substring(indexOfDecimal));
-        return (double) doubleAsString.substring(indexOfDecimal));
+        return Double.parseDouble(doubleAsString.substring(indexOfDecimal));
     }
-
+    public static void main(String args[]) {
+        MoonPhase next = new MoonPhase();
+        System.out.println("Next New Moon is " + next.calculateNextNewMoon().toString());
+    }
 }
